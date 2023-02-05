65564d169090462d34c8d50cfd2c2e13934b2fc1
Miguel Diaz <miguel.diazant@gmail.com>
2023-02-05 17:18:28 -0600

    add DisneyCharacter model and GetCharactersApiResponse

---
+++ Disneydex/DisneyCharacter.swift
@@ -0,0 +1,23 @@
+//
+//  DisneyCharacter.swift
+//  Disneydex
+//
+//  Created by Miguel Diaz on 2/5/23.
+//
+
+import Foundation
+
+struct DisneyCharacter {
+    var id: Int
+    var name: String
+    var imageUrl: URL
+    var url: URL
+    
+    var films: [String]
+    var shortFilms: [String]
+    var tvShows: [String]
+    var videoGames: [String]
+    var parkAttractions: [String]
+    var allies: [String]
+    var enemies: [String]
+}
---
+++ Disneydex/GetCharactersApiResponse.swift
@@ -0,0 +1,15 @@
+//
+//  GetCharactersApiResponse.swift
+//  Disneydex
+//
+//  Created by Miguel Diaz on 2/5/23.
+//
+
+import Foundation
+
+struct GetCharactersApiResponse {
+    var data: [DisneyCharacter]
+    var count: Int
+    var totalPages: Int
+    var nextPage: URL
+}
